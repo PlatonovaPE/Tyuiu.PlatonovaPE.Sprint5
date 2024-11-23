c5200f6e50857a383959ef18adbc1add0cf32db3
Полина Платонова <platonovaPE@outlook.com>
2024-11-24 00:30:51 +0500

    Task1

--- Tyuiu.PlatonovaPE.Sprint5.Task1.V24.Lib/DataService.cs
+++ Tyuiu.PlatonovaPE.Sprint5.Task1.V24.Lib/DataService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.PlatonovaPE.Sprint5.Task1.V24.Lib
@@ -12,34 +14,54 @@ namespace Tyuiu.PlatonovaPE.Sprint5.Task1.V24.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask1.txt";
-            FileInfo fileInfo = new FileInfo(path);
-            bool fileExists = fileInfo.Exists;
-            if (fileExists)
+            //stopValue++;
+
+            string ans = "";
+
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask1.txt" });
+
+            FileInfo f = new FileInfo(path);
+            if (f.Exists)
             {
                 File.Delete(path);
             }
-            double y;
-            string strY;
-            for (int x = startValue; x <= stopValue; x++)
+
+            double temp;
+
+            for (int i = startValue; i < stopValue; i++)
             {
-                y = Math.Round(((3 * Math.Cos(x)) / (4 * x - 0.5)) + Math.Sin(x) - 5 * x - 2, 2);
-                strY = Convert.ToString(y);
-                if (x != stopValue && (4 * x - 0.5) != 0)
+                temp = 4 * i - 0.5;
+                if (temp == 0)
                 {
-                    File.AppendAllText(path, strY + Environment.NewLine);
+                    ans += "0" + "\r\n";
+                    continue;
                 }
-                else if (x != stopValue && (4 * x - 0.5) == 0)
-                {
-                    y = 0;
-                    strY = Convert.ToString(y);
-                    File.AppendAllText(path, strY + Environment.NewLine);
-                }
-                else
-                {
-                    File.AppendAllText(path, strY);
-                }
+                ans += Math.Round(3 * Math.Cos(i) / temp + Math.Sin(i) - 5 * i - 2, 2) + "\n";
             }
+
+            temp = 4 * stopValue - 0.5;
+            if (temp == 0)
+            {
+                ans += "0";
+                goto RET;
+            }
+            ans += Math.Round(3 * Math.Cos(stopValue) / temp + Math.Sin(stopValue) - 5 * stopValue - 2, 2);
+
+
+        RET:
+            File.WriteAllText(path, ans);
+            /*
+            temp = 4 * stopValue - 0.5;
+
+            if (temp == 0)
+            {
+                File.AppendAllText(path, "0");
+
+                return path;
+            }
+
+            File.AppendAllText(path, Math.Round(3 * Math.Cos(stopValue) / temp + Math.Sin(stopValue) - 5 * stopValue - 2, 2) + "");*/
+
             return path;
         }
     }
